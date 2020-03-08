9dcc44c797232801d3632e3a9ba72e77c0f18324
ard902 <ard902@mail.usask.ca>
2020-03-08 16:12:03 -0600

    added some files i want to solve

---
+++ kattisSolutions/guessthedatastructure.py
@@ -0,0 +1,16 @@
+from collections import deque
+import heapq
+import sys
+
+def main():
+    ins=[]
+    for line in sys.stdin:
+        ins.append(line.strip().split())
+    ind=0
+    it=0
+    while ind<len(ins):
+        if len(ins[ind])==1:
+            it=int(ins[ind][0])
+        else:
+            for 
+main()    
