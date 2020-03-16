599abb6add5f2fc362495d59a998c3d5cc96059c
projectPythonator <ard902@mail.usask.ca>
2020-03-16 16:29:43 -0600

    Create READYprimsfaster.py

---
+++ cheatsheetstuff/graphAlgos/READYprimsfaster.py
@@ -0,0 +1,22 @@
+#mat can be a adj list or an adj mat but needs to work in this manner of hash table 
+def mstPrimsfaster():
+    global pq, taken, mat, disto, inf
+    taken=[False]*V
+    disto=[inf]*V
+    pq=[(0,0,0)]
+    msc=0
+    mst=[]
+    while pq and len(mst)<V:
+        w,v,u=h.heappop(pq)
+        if not taken[v]:
+            msc+=w
+            mst.append((u+1, v+1))
+            taken[v]=True
+            for nv in mat:
+                if nv==v or taken[nv]:
+                    continue
+                dist=mat[v][nv]
+                if dist>disto[nv]:
+                    continue
+                disto[nv]=dist
+                h.heappush(pq, (dist,nv,v))
