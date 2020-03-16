44a6b839991e8f401b9bf808a46a9159aa845737
projectPythonator <ard902@mail.usask.ca>
2020-03-16 16:35:41 -0600

    Update READYprimsfaster.py

--- cheatsheetstuff/graphAlgos/READYprimsfaster.py
+++ cheatsheetstuff/graphAlgos/READYprimsfaster.py
@@ -1,4 +1,5 @@
 #mat can be a adj list or an adj mat but needs to work in this manner of hash table 
+#tested on lostmap, cats, need to translate it to more problems later lots i just used kruskals in c++ ^-^
 def mstPrimsfaster():
     global pq, taken, mat, disto, inf
     taken=[False]*V
