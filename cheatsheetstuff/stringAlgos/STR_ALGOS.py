6eb0eb24fd9dda8fa588c1cb3d4d1af556bfe51c
projectPythonator <ard902@mail.usask.ca>
2020-06-05 11:15:18 -0600

    Update STR_ALGOS.py
    i put in suffix array construction as well as lcp that makes use of suffix array

--- cheatsheetstuff/stringAlgos/STR_ALGOS.py
+++ cheatsheetstuff/stringAlgos/STR_ALGOS.py
@@ -1,21 +1,19 @@
 #kmp works on id stringmatching, irepeatmyself, 
-
+#suffix sort tested on suffixsorting
+#longest common prefix repeatedsubstrings, dvaput
 
 class STR_ALGOS:
-    def __init__(self, s, offset):
-        self.n=len(s)
-        self.m=0
-        self.T=[c for c in s]
-        self.P=[]
-        self.B=[]
+    def __init__(self):
+        self.n=0
+        self.T=''
     
-    def reset_t(self, s):
+    def prepKMP(self, s):
         self.n=len(s)
-        self.T=[c for c in s]
+        self.T=s
     
     def kmpPreprocess(self, s):
         self.m=len(s)
-        self.P=[c for c in s]
+        self.P=s
         self.B=[0]*(self.m+1) #add plus one if no work
         self.B[0]=j=-1
         for i in range(self.m):
@@ -35,3 +33,76 @@ class STR_ALGOS:
                 ans.append(1+i-j)
                 j=self.B[j]
         return ans
+    
+    def prepSA(self, s):
+        k=1
+        self.T=s+chr(0)
+        self.n=len(self.T)
+        self.c=[0]*max(300,self.n)
+        self.tempSA=[0]*self.n
+        self.tempRA=[0]*self.n
+        self.RA=[ord(C) for C in self.T]
+        self.SA=[i for i in range(self.n)]
+        self.K=[]
+        while k<self.n:
+            self.K.append(k)
+            k*=2
+
+    def countingSort(self, k):
+        maxi=max(300,self.n)
+        tmp=0
+        for i in range(maxi):
+            self.c[i]=0
+        self.c[0]+=(self.n-(self.n-k))
+        for i in range(self.n-k):
+            self.c[self.RA[i+k]]+=1
+        for i in range(maxi):
+            t=self.c[i]
+            self.c[i]=tmp
+            tmp+=t
+        for i in range(self.n):
+            pos=0
+            if self.SA[i]+k<self.n:
+                pos=self.RA[self.SA[i]+k]
+            self.tempSA[self.c[pos]]=self.SA[i]
+            self.c[pos]+=1
+        for i, el in enumerate(self.tempSA):
+            self.SA[i]=el
+
+    def constructSA(self):
+        for k in self.K:
+            self.countingSort(k)
+            self.countingSort(0)
+            self.tempRA[self.SA[0]],r=0,0
+            for i in range(1, self.n):
+                s1=self.SA[i]
+                s2=self.SA[i-1]
+                if not (self.RA[s1] == self.RA[s2] and self.RA[s1+k] == self.RA[s2+k]):
+                    r+=1
+                self.tempRA[s1]=r
+            for i,el in enumerate(self.tempRA):
+                self.RA[i]=el
+            if self.RA[self.SA[-1]]==self.n-1:
+                break
+        self.tempRA=[]
+        self.tempSA=[]
+        self.RA=[]
+    
+    def computeLCP(self):
+        self.LCP=[0]*self.n
+        self.Phi=[0]*self.n
+        self.PLCP=[0]*self.n
+        self.Phi[0]=-1
+        L=0
+        for i in range(1, self.n):
+            self.Phi[self.SA[i]]=self.SA[i-1]
+        for i in range(self.n):
+            if self.Phi[i]==-1:
+                self.PLCP[i]=0
+                continue
+            while self.T[i+L]==self.T[self.Phi[i]+L]:
+                L+=1
+            self.PLCP[i]=L
+            L=max(L-1,0)
+        for i in range(self.n):
+            self.LCP[i]=self.PLCP[self.SA[i]]
