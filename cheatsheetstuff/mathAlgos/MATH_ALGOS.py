5eba9ec9f27434dd3870d8d75cda7354b06fd3ef
projectPythonator <ard902@mail.usask.ca>
2020-06-07 11:17:47 -0600

    Update MATH_ALGOS.py

--- cheatsheetstuff/mathAlgos/MATH_ALGOS.py
+++ cheatsheetstuff/mathAlgos/MATH_ALGOS.py
@@ -1,6 +1,8 @@
 #is_prime tested on primes2
 #lcm/gcd tested on temperatureconfusion, primes2, dasblinkenlights, jackpot, prsteni, smallestmultiple, jughard
 #extendedEuclid tested on candydistribution, modulararithmetic
+#
+#fib_logn tested on porpoises, anti11
 
 class MATH_ALGOS:
     def __init__(self):
@@ -59,3 +61,39 @@ class MATH_ALGOS:
         x,y,d=self.extendedEuclid(b, a%b)
         return y, x-y*(a//b), d
     
+    def fib_iter(self, n):
+        self.fibs=[0]*(n+1)
+        self.fibs[1]=1
+        for i in range(2, n+1):
+            self.fibs[i]=self.fibs[i-1]+self.fibs[i-2]
+        return self.fibs[n]
+    
+    def fib_logn_rec(self, n):
+        if n==0:
+            return 0
+        if n==1 or n==2:
+            self.fibs[n]=1
+            return 1
+        if n in self.fibs:
+            return self.fibs[n]
+        
+        if n&1:
+            k=(n+1)//2
+            fk1=self.fib_logn_rec(k)
+            fk2=self.fib_logn_rec(k-1)
+            self.fibs[n]=fk1*fk1+fk2*fk2
+        else:
+            k=n//2
+            fk1=self.fib_logn_rec(k)
+            fk2=self.fib_logn_rec(k-1)
+            self.fibs[n]=(2*fk2+fk1)*fk1
+        return self.fibs[n]
+        
+        
+    def fib_logn(self, n):
+        self.fibs={}
+        return self.fib_logn_rec(n)
+        
+
+obj=MATH_ALGOS()
+
