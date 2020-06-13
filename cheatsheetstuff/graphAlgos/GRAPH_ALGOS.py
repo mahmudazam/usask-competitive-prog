14a448280ca7b7c9b2437c3ce8a55d51d5ec7c6e
projectPythonator <ard902@mail.usask.ca>
2020-06-13 12:03:20 -0600

    tested APSP
    APSP is now tested in this class to work on APSP on kattis

--- cheatsheetstuff/graphAlgos/GRAPH_ALGOS.py
+++ cheatsheetstuff/graphAlgos/GRAPH_ALGOS.py
@@ -1,10 +1,16 @@
 from sys import stdin as rf
 #dijkstras tested shortestpath1, shortestpath2(modified works), flowerytrails(modified works), fulltank(modified)
+#APSP tested on allpairspath, kastenlauf(modified), arbitrage
 INF=1000000000
 
 class GRAPH_ALGOS():
     def __init__(self):
         self.adj={}
+    
+    def init_mat(self, n):
+        self.V=n
+        self.mat=[[INF]*n for _ in range(n)]
+        for i in range(n): self.mat[i][i]=0
     
     #modify the bottom two as needed
     def init_adj(self, n):
@@ -12,6 +18,10 @@ class GRAPH_ALGOS():
     
     def add_edge_adj(self, u, v, w):
         self.adj[u][v]=w
+        
+    def add_edge_mat(self, u, v, w):
+        self.mat[u][v]=min(w, self.mat[u][v])
+    
     
     def dijkstras(self, s, t):
         import heapq as h
@@ -32,4 +42,12 @@ class GRAPH_ALGOS():
         for k in range(self.V):
             for i in range(self.V):
                 for j in range(self.V):
-                    self.adjmat[i][j]=min(self.adjmat[i][j], self.adjmat[i][k]+self.adjmat[k][j])
+                    #if self.mat[i][k]<INF and self.mat[k][j]<INF: #use for negative cycles check
+                    self.mat[i][j]=min(self.mat[i][j], self.mat[i][k]+self.mat[k][j])
+    
+    def APSP_neg_cycles(self):
+        for i in range(self.V):
+            for j in range(self.V):
+                for k in range(self.V):
+                    if self.mat[k][k]<0 and self.mat[i][k]!=INF and self.mat[k][j]!=INF:
+                        self.mat[i][j]=-INF
