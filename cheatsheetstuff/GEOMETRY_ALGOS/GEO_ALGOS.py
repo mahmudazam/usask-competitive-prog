05e0a5e2e53882d5dbb534213cb64457694bdf0b
projectPythonator <ard902@mail.usask.ca>
2020-06-17 15:18:35 -0600

    added in a handful of functions
    circle stuff and finished off points and lines

--- cheatsheetstuff/GEOMETRY_ALGOS/GEO_ALGOS.py
+++ cheatsheetstuff/GEOMETRY_ALGOS/GEO_ALGOS.py
@@ -1,3 +1,4 @@
+
 import math
 from sys import stdin as rf
 
@@ -36,6 +37,8 @@ class GEO_ALGOS:
     def dot_product(self, a, b): return a.x*b.x+a.y*b.y #move to point class
     def cross_product(self, a, b): return a.x*b.y-a.y*b.x
     
+    def dist2(self, a, b): return self.dot_product(a-b,a-b)
+    
     def rotate_ccw90(self, p): return pt_xy(-p.y, p.x)
     def rotate_cw90(self, p):  return pt_xy( p.y,-p.x)
     def rotate_ccw(self, p, t): 
@@ -62,17 +65,51 @@ class GEO_ALGOS:
         return (abs(self.cross_product(b-a, c-d))<EPS)
         
     def is_lines_collinear(self, a, b, c, d): #copy paste code if its too slow this is safety from typing errors
-        return (self.lines_parallel(a,b,c,d) 
-        and self.lines_parallel(b,a,a,c) 
-        and self.lines_parallel(d,c,c,a))
+        return (self.is_lines_parallel(a,b,c,d) 
+        and self.is_lines_parallel(b,a,a,c) 
+        and self.is_lines_parallel(d,c,c,a))
     
+    #return if collinear to get a unique intersection or check outside the funtion before hand
     def is_segments_intersect(self, a, b, c, d):
         if self.is_lines_collinear(a, b, c, d):
             if self.dist2(a, c)<EPS or self.dist2(a, d)<EPS or self.dist2(b, c)<EPS or self.dist2(b, d)<EPS:
                 return True
-            return not(self.dot(c-a, c-b)>0 and self.dot(d-a, d-b)>0 and self.dot(c-b, d-b)>0)
-        ba=b-a
-        dc=d-c
-        if self.cross(d-a, ba)*self.cross(c-a, ba)>0: return False
-        return not(self.cross(a-c, dc)*self.cross(b-c, dc)>0)
+            return not(self.dot_product(c-a, c-b)>0 and self.dot_product(d-a, d-b)>0 and self.dot_product(c-b, d-b)>0)
+        return not((self.cross_product(d-a,b-a)*self.cross_product(c-a,b-a)>0)
+        or (self.cross_product(a-c,d-c)*self.cross_product(b-c,d-c)>0))
     
+    #move not outside the expr to get unique checker
+    def is_lines_intersect(self, a, b, c, d):
+        return (not self.is_lines_parallel(a,b,c,d) or self.is_lines_collinear(a,b,c,d))
+    
+    #this assumes you checked if unique intersections
+    #doubles as segment intersections if youve check that they actually intersect
+    def pt_lines_intersect(self, a, b, c, d):
+        b,c,d=b-a,c-a,c-d
+        return a+b*self.cross_product(c,d)/self.cross_product(b,d)
+        
+        
+    #add the case for lines collinear
+    def circle_center_given_abc(self, a, b, c):
+        b,c=(a+b)/2,(a+c)/2
+        return self.pt_lines_intersect(b, b+self.rotate_cw90(a-b), c, c+rotate_cw90(a-c))
+    
+    #mod for integer return later 
+    def circle_has_point(self, a, b, r): return (self.euclidean_distance(a, b)<r)
+    
+    def circle_line_intersection(self, a, b, c, r):
+        b,a=b-a,a-c
+        A,B,C=self.dot_product(b,b), self.dot_product(a,b), self.dot_product(a,a)-r*r
+        D=B*B-A*C
+        if D<-EPS: return None
+        rVal=c+a+b*(-B+math.sqrt(D+EPS))/A
+        return (rVal) if not(D>EPS) else (rVal, c+a+b*(-B-math.sqrt(D))/A)
+        
+    def circle_circle_intersection(self, a, b, r1, r2):
+        d=self.euclidean_distance(a, b)
+        if d>r1+r2 or d+min(r1, r2)<max(r1,r2): return None
+        x=(d*d-r2*r2+r1*r1)/(2*d)
+        y=math.sqrt(r1*r1-x*x)
+        v=(b-a)/d
+        i,j=a+v*x,self.rotate_ccw90(v)*y
+        return (i+j) if not(y>0): else (i+j, i-j)
